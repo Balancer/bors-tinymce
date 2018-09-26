d2ac573dc50780213766c6a164c5622b55091e2f
Balancer <balancer@balancer.ru>
2018-09-26 06:12:03 +0300

    ++

--- Tinymce.php
+++ Tinymce.php
@@ -4,13 +4,15 @@ namespace B2;
 
 class Tinymce
 {
-	static function appear($element, $params = array())
+	static function appear($element = "'.tinymce'", $params = [])
 	{
+		// CDN: https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.13/jquery.tinymce.min.js
 		bors_use('/_bower-assets/tinymce/jquery.tinymce.min.js');
 
 		template_js('
 $().ready(function() { $("'.$element.'").tinymce({
-	script_url : "/_bower-assets/tinymce/tiny_mce.js", '.file_get_contents(BORS_3RD_PARTY.'/inc/tinymce/'.$mode.'.inc').' }); });');
-
-');
+//	script_url : "/_bower-assets/tinymce/tiny_mce.js", '.@file_get_contents(BORS_3RD_PARTY.'/inc/tinymce/'.@$mode.'.inc').'
+	});
+});');
+	}
 }
